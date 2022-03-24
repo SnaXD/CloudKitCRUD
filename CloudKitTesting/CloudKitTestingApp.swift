057489a74b409df2eddd1763ad36dc129a51d985
Jonathan T. Nielsen <44645695+SnaXD@users.noreply.github.com>
2022-03-24 11:07:04 +0100

    Initial Commit

---
+++ CloudKitTesting/CloudKitTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudKitTestingApp.swift
+//  CloudKitTesting
+//
+//  Created by Jonathan T. Nielsen on 24/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CloudKitTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
